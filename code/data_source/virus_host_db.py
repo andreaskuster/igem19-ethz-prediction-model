587083d67557236b5f6d65bed0e1bcf6852dcfd5
andreas <mail@andreaskuster.ch>
2019-07-24 16:15:32 +0200

    Add initial tsv read capabilities. Add option to prevent re-download of files if they are already present.

--- code/data_source/virus_host_db.py
+++ code/data_source/virus_host_db.py
@@ -1,4 +1,6 @@
 import os
+import csv
+
 from ftplib import FTP
 
 from data_source import AbstractDataSource
@@ -15,23 +17,34 @@ class VirusHostDB(AbstractDataSource):
 
     _FTP_BASE_URI = "ftp.genome.jp"
     _FILE_DIRECTORY = "/pub/db/virushostdb/"
-    _FILES = ["README", "virushostdb.tsv"]
+    _README_FILE = "README"
+    _DATA_FILE = "virushostdb.tsv"
+    _FILES = [_README_FILE, _DATA_FILE]
     _STORAGE_DIR = "data/"
     _FILE_PREFIX = "virus_host_db_"
+    _DATA_PATH = os.path.join(_STORAGE_DIR, _FILE_PREFIX + _DATA_FILE)
 
-    def fetch_data(self):
-        with FTP(self._FTP_BASE_URI) as ftp:
-            ftp.login(user="anonymous")
-            ftp.cwd(self._FILE_DIRECTORY)
-            director_content = ftp.nlst()
-            for file in self._FILES:
-                if file in director_content:
-                    with open(os.path.join(self._STORAGE_DIR, self._FILE_PREFIX + file), 'wb') as f:
-                        ftp.retrbinary('RETR ' + file, f.write)
-                else:
-                    raise RuntimeError("File {} does not exist on the Virus Host DB server.")
+    def fetch_data(self, skip_if_exists = True):
+        if not os.path.exists(self._DATA_PATH):
+            with FTP(self._FTP_BASE_URI) as ftp:
+                ftp.login(user="anonymous")
+                ftp.cwd(self._FILE_DIRECTORY)
+                director_content = ftp.nlst()
+                for file in self._FILES:
+                    if file in director_content:
+                        with open(self._DATA_PATH, 'wb') as f:
+                            ftp.retrbinary('RETR ' + file, f.write)
+                    else:
+                        raise RuntimeError("File {} does not exist on the Virus Host DB server.")
+
+    def read_data(self):
+        with open(self._DATA_PATH, "r") as tsvin:
+            tsvin = csv.reader(tsvin, delimiter='\t')
+            for row in tsvin:
+                print(row)
 
 
 if __name__ == "__main__":
     source = VirusHostDB()
-    source.fetch_data()
+    source.fetch_data(skip_if_exists=True)
+    source.read_data()
