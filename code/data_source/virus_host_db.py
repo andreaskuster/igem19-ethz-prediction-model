3417fa9f4873e8c19589f74634ce15102965fab5
andreas <mail@andreaskuster.ch>
2019-07-24 16:02:44 +0200

    Implement the data fetching from the virus host db ftp server. Add abstract superclass.

--- code/data_source/virus_host_db.py
+++ code/data_source/virus_host_db.py
@@ -1,21 +1,37 @@
+import os
 from ftplib import FTP
 
-"""
+from data_source import AbstractDataSource
 
 """
 
-_README_URI = "ftp.genome.jp"
+"""
 
 
-def fetch_data():
-    with FTP(_README_URI) as ftp:
-        ftp.login(user="anonymous")
-        ftp.cwd("/pub/db/virushostdb/")
-        test = ftp.nlst()
-        with open("data/README", 'wb') as f:
-            ftp.retrbinary('RETR ' + "README", f.write)
+class VirusHostDB(AbstractDataSource):
+
+    def __init__(self):
+        super().__init__()
+
+    _FTP_BASE_URI = "ftp.genome.jp"
+    _FILE_DIRECTORY = "/pub/db/virushostdb/"
+    _FILES = ["README", "virushostdb.tsv"]
+    _STORAGE_DIR = "data/"
+    _FILE_PREFIX = "virus_host_db_"
+
+    def fetch_data(self):
+        with FTP(self._FTP_BASE_URI) as ftp:
+            ftp.login(user="anonymous")
+            ftp.cwd(self._FILE_DIRECTORY)
+            director_content = ftp.nlst()
+            for file in self._FILES:
+                if file in director_content:
+                    with open(os.path.join(self._STORAGE_DIR, self._FILE_PREFIX + file), 'wb') as f:
+                        ftp.retrbinary('RETR ' + file, f.write)
+                else:
+                    raise RuntimeError("File {} does not exist on the Virus Host DB server.")
 
 
 if __name__ == "__main__":
-    fetch_data()
-    print("done")
+    source = VirusHostDB()
+    source.fetch_data()
